eed11369dcbddca6ea14782acd0e9b0e91d891f6
Shannon Moranetz <shannonrmoranetz@gmail.com>
2018-11-03 22:44:11 -0600

    Favorite semi broken

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ document.getElementById('add-button').addEventListener('click', setProperties);
 reloadCards();
 
 function reloadCards() {
-    Object.keys(localStorage).forEach(function(key) {
-      addCard(JSON.parse(localStorage.getItem(key)));
+  Object.keys(localStorage).forEach(function(key) {
+    addCard(JSON.parse(localStorage.getItem(key)));
   })
 };
 
@@ -25,16 +25,17 @@ function addCard(photo) {
   card.className = 'photo-card';
   card.innerHTML = 
   `<div class="card-content">
-      <h2 class="card-title" id="${photo.id}" contenteditable= "true">${photo.title}</h2>
-      <h4 class="card-caption" contenteditable="true">${photo.caption}</h4>
-      <img class="card-image" src=${photo.file}>
-    </div>
-    <div class="card-bottom">
-      <img class="delete-icon card-icon" src="images/delete.svg">
-      <img class="favorite-icon card-icon" src="images/favorite.svg">
-    </div>
-`
-    cardSection.insertBefore(card, cardSection.firstChild); 
+  <h2 class="card-title" id="${photo.id}" contenteditable= "true">${photo.title}</h2>
+  <h4 class="card-caption" contenteditable="true">${photo.caption}</h4>
+  <img class="card-image" src=${photo.file}>
+  </div>
+  <div class="card-bottom">
+  <img class="delete-icon card-icon" src="images/delete.svg">
+
+  <img class="favorite-icon card-icon" src="${elem.src}">
+  </div>
+  `
+  cardSection.insertBefore(card, cardSection.firstChild); 
 }
 
 document.querySelector('.card-section').addEventListener('click', removeCard);
@@ -54,52 +55,52 @@ function removeCard(e) {
 document.querySelector('.card-section').addEventListener('focusout', updateCard);
 
 function updateCard(e) {
-    var id = e.target.closest('.photo-card').firstChild.firstChild.nextSibling.id;
-    var json = localStorage.getItem(id);
-    var photoObj = JSON.parse(json);
-    var {id, title, caption, file, favorite} = photoObj;
-    var photo = new Photo(id, title, caption, file, favorite);
+  var id = e.target.closest('.photo-card').firstChild.firstChild.nextSibling.id;
+  var json = localStorage.getItem(id);
+  var photoObj = JSON.parse(json);
+  var {id, title, caption, file, favorite} = photoObj;
+  var photo = new Photo(id, title, caption, file, favorite);
 
-     if (e.target.className === 'card-title') {
-      photo.updatePhoto(e.target.innerText, 'title');
-      console.log(e.target.innerText);
-    }
-    if (e.target.className === 'card-caption') {
-      photo.updatePhoto(e.target.innerText, 'caption');
-    }
+  if (e.target.className === 'card-title') {
+    photo.updatePhoto(e.target.innerText, 'title');
+    console.log(e.target.innerText);
+  }
+  if (e.target.className === 'card-caption') {
+    photo.updatePhoto(e.target.innerText, 'caption');
+  }
 };
 
 document.querySelector('.card-section').addEventListener('click', favorite);
 
 function favorite(e) {
-    var id = e.target.closest('.photo-card').firstChild.firstChild.nextSibling.id;
-    var json = localStorage.getItem(id);
-    var photoObj = JSON.parse(json);
-    var {id, title, caption, file, favorite} = photoObj;
-    var photo = new Photo(id, title, caption, file, favorite);
+  var id = e.target.closest('.photo-card').firstChild.firstChild.nextSibling.id;
+  var json = localStorage.getItem(id);
+  var photoObj = JSON.parse(json);
+  var {id, title, caption, file, favorite} = photoObj;
+  var photo = new Photo(id, title, caption, file, favorite);
 
-      if(photo.favorite === false) {
-        photo.favorite = true;
-        photo.saveToStorage();
-        changeImage();
-      }
-      else {
-        photo.favorite = false;
-        photo.saveToStorage();
-        changeImage();
-      }
+  if(photo.favorite === false) {
+    photo.favorite = true;
+    photo.saveToStorage();
+    changeImage(e, photo);        
+  }
+  else {
+    photo.favorite = false;   
+    photo.saveToStorage();
+    changeImage(e, photo);        
+  }
 }
 
-function changeImage() {
-   if (document.querySelector('.favorite-icon').src === "images/favorite.svg") 
-        {
-            document.getElementById('.favorite-icon').src = "images/favorite-active";
-        }
-    else 
-        {
-            document.getElementById('.favorite-icon').src = "images/favorite.svg";
-        }
+function changeImage(e, photo) {
+  var elem = e.target.closest('.card-section').firstChild.firstChild.nextSibling.nextSibling.firstChild.nextSibling.nextSibling.nextSibling;
+  if (photo.favorite) {
+    elem.src = "images/favorite-active.svg";
+  } else {
+    elem.src = "images/favorite.svg";
+  }
 }
+
+
 
 
 
